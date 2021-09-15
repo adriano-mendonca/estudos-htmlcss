7835afc615318f6dd588d4688389ddcd397e35b1
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2021-09-14 21:48:14 -0300

    finalização do projeto

---
+++ origamid/bikcraft/website/js/script.js
@@ -0,0 +1,22 @@
+
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        nav: false, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: false, // pausa a transição automática
+    });
+
+    new SimpleSlide({
+        slide: 'portfolio', // nome do atributo data-slide="principal"
+        nav: true, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: false, // pausa a transição automática
+    });
+}
+
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
